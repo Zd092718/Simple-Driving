813afd043e4a9cc225b02032c79df13696c076c7
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-20 09:52:54 -0600

    Initial commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float speedGainPerSecond = .2f;
+    [SerializeField] float turnSpeed = 200f;
+
+    int steerValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed += speedGainPerSecond * Time.deltaTime;
+
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
+    }
+}
