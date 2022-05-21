d6c44b0f1ff90f4393829a9564e48a2ac1af6cfa
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-21 13:38:26 -0600

    Energy system in place

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,14 +9,57 @@ public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] TMP_Text highScoreText;
+    [SerializeField] TMP_Text energyText;
+    [SerializeField] int maxEnergy;
+    [SerializeField] int energyRechargeDuration;
+
+    int energy;
+
+    const string EnergyKey = "Energy";
+    const string EnergyReadyKey = "Energy Ready";
 
     private void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
         highScoreText.text = $"High Score: {highScore}";
+
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+
+        if (energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+
+            if (energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if (DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, maxEnergy);
+            }
+        }
+
+        energyText.text = $"Play ({energy})";
     }
     public void Play()
     {
+        if (energy < 1)
+        {
+            return;
+        }
+
+        energy--;
+
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if (energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(1);
+            PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
+        }
+
         SceneManager.LoadScene(1);
+
     }
 }
